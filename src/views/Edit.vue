17604450f9bcd5b73ae498a5b4a7c48a55028624
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-26 13:14:40 +0200

    Add Edit view
    Added template with styles.
    Added dummy hardcoded user data.

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="edit-view">
-    <h1>Edit trainee</h1>
+  <div class="add-view container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center text-md-left text-secondary">Edit trainee</h1>
+      </div>
+    </div>
+    <UserForm :userData="userData" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import UserForm from '@/components/User-form';
 
 export default {
-  name: 'Edit'
+  name: 'User',
+  components: { UserForm },
+  data() {
+    return {
+      userData: {
+        id: 2,
+        email: 'janet.weaver@reqres.in',
+        first_name: 'Janet',
+        last_name: 'Weaver',
+        avatar:
+          'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg'
+      }
+    };
+  }
 };
 </script>
