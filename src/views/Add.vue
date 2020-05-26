dd3623b9579ca136a9ae4879b9c411676abdfcad
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-26 13:13:39 +0200

    Add Add view content
    Added template with styles.

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="add-view">
-    <h1>Add trainee</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center text-md-left text-secondary">Add trainee</h1>
+      </div>
+    </div>
+    <UserForm />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import UserForm from '@/components/User-form';
 
 export default {
-  name: 'Add'
+  name: 'Add',
+  components: { UserForm }
 };
 </script>
