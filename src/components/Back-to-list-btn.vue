71333bba633200fa0f2d6453692e52278824f529
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-26 16:50:49 +0200

    Add Back-to-list-btn.vue
    Added component with template & styles.

---
+++ src/components/Back-to-list-btn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row">
+    <div class="col-12 d-flex">
+      <router-link class="btn btn-secondary mt-4" to="/"
+        ><svg
+          class="bi bi-arrow-left"
+          width="1.5rem"
+          height="1.5rem"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.854 4.646a.5.5 0 0 1 0 .708L3.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M2.5 8a.5.5 0 0 1 .5-.5h10.5a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
+        <span class="ml-2">Back to trainees list</span>
+      </router-link>
+    </div>
+  </div>
+</template>
