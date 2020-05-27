e01dfce9d81cd14b86c01b2ad73aa982738c110d
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-27 17:02:07 +0200

    Add logic for Add & Edit user actions
    Added smiple error handling.

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import eventBus from '../eventBus';
 
 Vue.use(VueAxios, axios);
 Vue.axios.defaults.baseURL = 'https://reqres.in/api';
@@ -19,10 +20,49 @@ const mutations = {
 
 const actions = {
   fetchUsersList(page = 1) {
-    Vue.axios.get(`/users?page=${page}`).then(res => {
-      // console.log(res);
-      mutations.setUsersList(res.data);
-    });
+    Vue.axios
+      .get(`/users?page=${page}`)
+      .then(res => {
+        if (res.status === 200) {
+          eventBus.$emit('hideModal'); // listenesr: App.vue
+          mutations.setUsersList(res.data);
+        } else throw new Error('Fetching trainees failed!');
+      })
+      .catch(err => console.log(err));
+  },
+  addUser(newUser) {
+    eventBus.$emit('showModal'); // listeners: App.vue
+    const body = {
+      name: newUser.first_name,
+      job: newUser.last_name
+    };
+
+    Vue.axios
+      .post('/users', body)
+      .then(res => {
+        console.log(res.status);
+        if (res.status === 201) {
+          eventBus.$emit('showModal', 'userAdded'); // listeners: App.vue
+        } else throw new Error('Adding trainee failed!');
+      })
+      .catch(err => console.log(err));
+  },
+  editUser(newData, id) {
+    eventBus.$emit('showModal'); // listeners: App.vue
+    const body = {
+      name: newData.first_name,
+      job: newData.last_name
+    };
+
+    Vue.axios
+      .put(`/users/${id}`, body)
+      .then(res => {
+        console.log(res.status);
+        if (res.status === 20) {
+          eventBus.$emit('showModal', 'userEdited'); // listeners: App.vue
+        } else throw new Error('Updating trainee failed!');
+      })
+      .catch(err => console.log(err));
   }
 };
 
