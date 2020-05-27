af867bb1b04b7d176376e2a233180f0ac528a93a
Kamil Sztefko <sztefkokamil@gmail.com>
2020-05-27 16:59:50 +0200

    Add form submit logic

--- src/components/User-form.vue
+++ src/components/User-form.vue
@@ -1,11 +1,12 @@
 <template>
-  <form class="row">
+  <form class="row" @submit="submitForm">
     <div class="col-12 col-md-4 my-3 order-md-2">
       <div class="card h-100 py-4 shadow-sm border-0">
         <div class="card-body">
           <img
             class="m-4"
             width="128"
+            style="min-width:128px;"
             :src="
               avatar ||
                 'https://theglutensummit.com/wp-content/uploads/2013/11/mystery-avatar2-200x200.png'
@@ -58,6 +59,9 @@
 </template>
 
 <script>
+import { actions } from '../store/store';
+import eventBus from '../eventBus';
+
 export default {
   props: { userData: { type: Object, default: () => null } },
   data() {
@@ -68,10 +72,36 @@ export default {
     };
   },
   created() {
-    if (this.userData) {
-      this.firstname = this.userData.first_name;
-      this.lastname = this.userData.last_name;
-      this.avatar = this.userData.avatar;
+    this.checkFormType();
+    eventBus.$on('showModal', infoType => {
+      if (infoType === 'userAdded') this.clearForm();
+    });
+  },
+  methods: {
+    ...actions,
+    checkFormType() {
+      if (this.userData) {
+        this.firstname = this.userData.first_name;
+        this.lastname = this.userData.last_name;
+        this.avatar = this.userData.avatar;
+      }
+    },
+    submitForm(event) {
+      event.preventDefault();
+
+      const data = {
+        first_name: this.firstname,
+        last_name: this.lastname,
+        avatar: this.avatar
+      };
+
+      if (this.userData) this.editUser(data, this.userData.id);
+      else this.addUser(data);
+    },
+    clearForm() {
+      this.firstname = '';
+      this.lastname = '';
+      this.avatar = '';
     }
   }
 };
